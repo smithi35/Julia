06fc9ce9e2c0db2efd3dad3c1ef1308a72399faf
Ian Smith <ian_smith_12a@mymts.net>
2016-03-06 09:09:27 -0600

    Prints an image, not sure it is the right one yet

--- Julia.py
+++ Julia.py
@@ -2,32 +2,35 @@ from tkinter import *
 import math
 
 def draw_set(top, maxModules, maxIterations, c):
-	value = 120
-	delta = 1 / value
+	value = 100
 	canvas = Canvas(top, bg="white", height=value, width=value)
+	img = PhotoImage(width=value, height=value)
+	canvas.create_image((0, 0), image=img, state="normal")
 	canvas.pack(fill=BOTH, expand=YES)
 	
+	delta = 4 / value
 	pixelx = 0
-	complexx = -2+2j
+	complexx = -2
 	while pixelx < value:
 		pixely = 0
-		complexy = -2+2j
+		complexy = 2
 		while pixely < value: 
 			# print("x = " + str(pixelx) + "\ny = " + str(pixely))
 			# for each x and y value caluculate z 
 			iterations = iterate(complexx, complexy, maxModules, maxIterations, c)
-			print("Iterations = " + str(iterations))
+			# print("Iterations = " + str(iterations))
 			col = colorZ(iterations,maxIterations)
-			print("col = " + str(col))
+			# print("col = " + str(col))
+			
+			# create a pixel at color: col at (pixelx, pixely)
+			img.put(col, (pixelx, pixely))
 		
 			pixely = pixely + 1
 			complexy = complexy - delta
 
 		pixelx = pixelx + 1
 		complexx = complexx + delta
-	exit()
-	
-	return top
+	top.mainloop()
 
 def iterate(x, y, maxModules, maxIterations, c):
 	count = 0
@@ -42,7 +45,7 @@ def iterate(x, y, maxModules, maxIterations, c):
 		
 		count = count + 1
 
-	print("count = " + str(count))
+	# print("count = " + str(count))
 	# print("x = " + str(x))
 	# print("y = " + str(y))
 
@@ -58,13 +61,22 @@ def colorZ(z, maxIterations):
 		col = "white"
 	else:
 		# start with white, increment according to z
-		icol = 0xffffff
-		delta = int(icol / maxIterations)
-		i = 0
-		while i < z:
-			icol = icol - delta
-			i = i + 1
-		col = hex(icol)
+		icol = [0xff, 0xff, 0xff]
+		col = "#"
+		
+		delta = int(icol[0] / maxIterations)
+		for i in range(3):
+			j = 0
+			while j < z:
+				icol[i] = icol[i] - delta
+				j = j + 1
+			# append icol[i] to col
+			out = hex(icol[i]).upper()
+			out = "{0:02X}".format(icol[i])
+			col = col + out
+
+		# col = hex(icol).upper()
+		# col = format(col, 'x')
 	# print("col = " + str(col))
 
 	return col
@@ -77,7 +89,7 @@ def main():
 	maxIterations = 50
 
 	c = -0.996 + 0.252j
-	top = draw_set(top, maxModules, maxIterations, c)
+	draw_set(top, maxModules, maxIterations, c)
 	# c = -0.271 - 0.702j
 	# draw_set(top, maxModules, maxIterations, c)
 	# c = 0.522 - 0.53j
@@ -87,6 +99,6 @@ def main():
 	# c = 0.19 + 0.98j
 	# draw_set(top, maxModules, maxIterations, c)
 	
-	top.mainloop()
+	# top.mainloop()
 
 main()
