32505595be501b835a695f7d61e35b9f7b7a169b
Ian Smith <ian_smith_12a@mymts.net>
2016-03-06 18:42:01 -0600

    added a basic click method, I have also decided to pass around a function pointer for determining g(z)

--- Julia.py
+++ Julia.py
@@ -1,23 +1,33 @@
-from tkinter import *
+from tkinter import Tk, Canvas, PhotoImage, mainloop
 import math
 
+# zoom in on the location indicated by the event
+# modify the maxModules and maxIterations accordingly
+def click(event) :
+	global maxModules, maxIterations
+	print("x = " + str(event.x) + ", y = " + str(event.y))
+
 def draw_set(top, maxModules, maxIterations, c):
-	value = 100
-	canvas = Canvas(top, bg="white", height=value, width=value)
+	top = Tk()
+	value = 300
+	canvas = Canvas(top, height=value, width=value)
+	canvas.bind("<Button-1>", click)
 	img = PhotoImage(width=value, height=value)
 	canvas.create_image((0, 0), image=img, state="normal")
-	canvas.pack(fill=BOTH, expand=YES)
+	canvas.pack()
 	
-	delta = 4 / value
+	deltax = 4 / value
+	deltay = 4j / value
 	pixelx = 0
-	complexx = -2
-	while pixelx < value:
+	complexx = -4
+	while pixelx <= value:
 		pixely = 0
-		complexy = 2
-		while pixely < value: 
+		complexy = 4j
+		while pixely <= value: 
+			complex = complexx + complexy
 			# print("x = " + str(pixelx) + "\ny = " + str(pixely))
 			# for each x and y value caluculate z 
-			iterations = iterate(complexx, complexy, maxModules, maxIterations, c)
+			iterations = iterate(complex, maxModules, maxIterations, c)
 			# print("Iterations = " + str(iterations))
 			col = colorZ(iterations,maxIterations)
 			# print("col = " + str(col))
@@ -26,21 +36,22 @@ def draw_set(top, maxModules, maxIterations, c):
 			img.put(col, (pixelx, pixely))
 		
 			pixely = pixely + 1
-			complexy = complexy - delta
+			complexy = complexy - deltay
 
 		pixelx = pixelx + 1
-		complexx = complexx + delta
+		complexx = complexx + deltax
+		# print("complexx = " + str(complexx) + ", complexy = " + str(complexy))
+	
 	top.mainloop()
 
-def iterate(x, y, maxModules, maxIterations, c):
+def iterate(complex, maxModules, maxIterations, c):
 	count = 0
 	do_continue = 1
 	# print("c = " + str(c))
 	# print("maxModules = " + str(maxModules))
 
-	while (count < maxIterations) and ((math.fabs(x.real) < maxModules) and (math.fabs(y.real) < maxModules)):
-		x = (x * x) + c
-		y = (y * y) + c
+	while (count < maxIterations) and (math.fabs(complex.real) < maxModules):
+		complex = (complex * complex) + c
 		# print("x = " + str(x))
 		
 		count = count + 1
@@ -83,15 +94,17 @@ def colorZ(z, maxIterations):
 	
 # need to map to plane
 def main():
-	top = Tk()
+	# top = Tk()
 
 	maxModules = 2.0
 	maxIterations = 50
 
+	# pass a function pointer instead of constant
 	c = -0.996 + 0.252j
-	draw_set(top, maxModules, maxIterations, c)
-	# c = -0.271 - 0.702j
-	# draw_set(top, maxModules, maxIterations, c)
+	# c = 1
+	draw_set(None, maxModules, maxIterations, c)
+	c = -0.271 - 0.702j
+	draw_set(None, maxModules, maxIterations, c)
 	# c = 0.522 - 0.53j
 	# draw_set(top, maxModules, maxIterations, c)
 	# c = 0.05 - 0.139j
